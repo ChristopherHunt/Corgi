6e65b0f98bcda6a9dbe991a25a7827611a364b99
Chris <Wash@Wash.local>
2016-03-24 23:31:49 -0700

    Added some content to handle the argv arguements that were being passed to these programs.

---
+++ Proto/cache_node_main.cpp
@@ -0,0 +1,23 @@
+#include <mpi.h>
+#include <stdio.h>
+#include "cache_node.h"
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+
+    printf("cache_node argc: %d\n", argc);
+    for (int i = 0; i < argc; ++i) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+
+    std::string mapping(argv[1]);
+    replace_commas(mapping);
+    std::vector<uint32_t> map_vec;
+    stringlist_to_vector(map_vec, mapping);
+
+    CacheNode cache_node(map_vec);
+
+    MPI_Finalize();
+
+    return 0;
+}
---
+++ Proto/job_node_main.cpp
@@ -0,0 +1,39 @@
+#include <mpi.h>
+#include "network_header.h"
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+
+    printf("job_node argc: %d\n", argc);
+    for (int i = 0; i < argc; ++i) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+
+    std::string mapping(argv[1]);
+    replace_commas(mapping);
+    std::vector<uint32_t> map_vec;
+    stringlist_to_vector(map_vec, mapping);
+
+    int local_size;
+    int local_rank;
+
+    MPI_Comm parent_comm;
+    int parent_size;
+    int parent_rank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &local_size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &local_rank);
+
+    MPI_Comm_get_parent(&parent_comm);
+    MPI_Comm_remote_size(parent_comm, &parent_size);
+    MPI_Comm_rank(parent_comm, &parent_rank);
+
+    printf("Job node: local rank - %d/%d parent rank - %d/%d\n", local_rank,
+            local_size, parent_rank, parent_size);
+
+    printf("Job node %d: team cache node: %d\n", local_rank, map_vec[local_rank]);
+
+    MPI_Finalize();
+
+    return 0;
+}
